40ba0b33dbf285f8ce6fc12e6a2dc57d5f3948c5
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2024-08-07 12:19:01 +0000

    Initial Commit

---
+++ tests/test_app.py
@@ -0,0 +1,22 @@
+import logging
+from unittest import TestCase
+import os
+import app
+
+class TestApp(TestCase):
+    """Account Service Tests"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Run once before all tests"""
+        
+
+    @classmethod
+    def tearDownClass(cls):
+        """Runs once before test suite"""
+
+    def setUp(self):
+        """Runs before each test"""
+
+    def tearDown(self):
+        """Runs once after each test case"""
