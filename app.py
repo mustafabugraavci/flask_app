de44a0a2c88d7308bfd2192edade548ab5e700ca
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2024-08-07 11:02:45 +0000

    Initial Commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+Bootstrap(app)
+
+class UploadForm(FlaskForm):
+    file = FileField('Upload Image', validators=[DataRequired()])
+    submit = SubmitField('Analyze')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('result', filename=filename))
+    return render_template('index.html', form=form)
+
+@app.route('/result/<filename>')
+def result(filename):
+    # Add AI model analysis here
+    # For now, just display the uploaded image
+    return render_template('result.html', filename=filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
